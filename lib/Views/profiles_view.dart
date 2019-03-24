22521adf6f6546b276310dd76bf39ba618620680
hbock-42 <hbock@student.42.fr>
2019-03-25 00:13:51 +0100

    fade out

--- lib/Views/profiles_view.dart
+++ lib/Views/profiles_view.dart
@@ -1,59 +1,98 @@
 import 'package:flutter/material.dart';
 
-class ProfilesPage extends StatefulWidget {
+List<Widget> SingleProfiles;
 
+class ProfilesPage extends StatefulWidget {
   @override
   _ProfilesPageState createState() => _ProfilesPageState();
 }
 
 class _ProfilesPageState extends State<ProfilesPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    
+    SingleProfiles = [
+      SingleProfile(),
+      SingleProfile(),
+      SingleProfile(),
+      SingleProfile(),
+      SingleProfile(),
+    ];
     return Container(
       margin: EdgeInsets.all(8),
       child: ListView(
-        children: [
-          SingleProfile(),
-          SingleProfile(),
-          SingleProfile(),
-          SingleProfile(),
-          SingleProfile(),
-        ],
+        children: SingleProfiles,
       ),
     );
   }
 }
 
-class SingleProfile extends StatelessWidget
-{
+enum WidgetVisibilty { Visible, FadingOut, FadingIn, Hidden }
+
+class SingleProfile extends StatefulWidget {
+  GlobalKey gKey = GlobalKey();
+  WidgetVisibilty visibility = WidgetVisibilty.Hidden;
+  Duration duration = Duration(days: 0);
+  double opacity = 1;
+
+  @override
+  _SingleProfileState createState() => _SingleProfileState();
+}
+
+class _SingleProfileState extends State<SingleProfile> {
+
+  Offset size() {
+      final RenderBox renderBox = this.widget.gKey.currentContext.findRenderObject();
+      return renderBox.localToGlobal(Offset.zero);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 9),
-      child: Container(
-        color: Color.fromRGBO(0, 0, 255, 0.8),
+    this.widget.visibility = WidgetVisibilty.Visible;
+    return GestureDetector(
+      key: this.widget.gKey,
+      onTap: () => _onSingleProfileWidgetTaped(this.widget.gKey),
+      child: AnimatedOpacity(
+        opacity: this.widget.opacity,
+        duration: this.widget.duration,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/images/hbock.jpg',
-                width: 80,
-                height: 80,
-              )
-            ],
+          padding: const EdgeInsets.only(bottom: 9),
+          child: Container(
+            color: Color.fromRGBO(0, 0, 255, 0.8),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/hbock.jpg',
+                    width: 80,
+                    height: 80,
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
+  }
+
+  _onSingleProfileWidgetTaped(GlobalKey<State<StatefulWidget>> key) {
+    setState(() {
+      final RenderBox renderBox = key.currentContext.findRenderObject();
+      for (var profiles in SingleProfiles) {
+        var p = profiles as SingleProfile;
+        if (p.gKey == key) {
+          if (p.visibility == WidgetVisibilty.Visible) {
+            p.duration = Duration(milliseconds: 500);
+            // todo create new widget
+          }
+        } else {
+          p.duration = Duration(milliseconds: 500);
+        }
+        p.opacity = 0;
+        print(renderBox.localToGlobal(Offset.zero));
+      }
+    });
   }
 }
